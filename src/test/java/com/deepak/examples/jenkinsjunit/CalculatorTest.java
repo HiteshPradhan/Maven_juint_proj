55c3b8f7ca647224f936706624d89221773b706f
HiteshPradhan <mikepradhan21@gmail.com>
2023-03-09 14:22:48 +0530

    test class change

--- src/test/java/com/deepak/examples/jenkinsjunit/CalculatorTest.java
+++ src/test/java/com/deepak/examples/jenkinsjunit/CalculatorTest.java
@@ -28,7 +28,7 @@ public class CalculatorTest {
 
 	@After
 	public void afterEachTest() {
-		System.out.println("hello world");
+		System.out.println("This is executed before each Test");
 	}
 
 	@Test
